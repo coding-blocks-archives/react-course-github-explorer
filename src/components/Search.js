30aa9a09650be062593346fd651263309321ce78
Naman Kumar <naman@outlook.in>
2020-04-15 00:54:41 +0530

    Handle Errors

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,22 +1,47 @@
 import React from "react";
 
 class Search extends React.Component {
-  state= {
-    username: '',
-  }
+  state = {
+    username: ""
+  };
 
   handleUserNameChange = e => {
-    const  value = e.target.value;
+    const value = e.target.value;
 
     this.setState({
-      username: value,
+      username: value
     });
   };
 
   render() {
+    const { fetchData } = this.props;
     const { username } = this.state;
 
-    return <input value={username} onChange={this.handleUserNameChange} type="text" name="username" placeholder="Enter username" />;
+    return (
+      <div className="bg-dark">
+        <div className="container py-5">
+          <div className="row">
+            <div className="col-8 offset-2 text-center">
+              <div className="row">
+                <div className="col-9">
+                  <input
+                    className="form-control"
+                    value={username}
+                    onChange={this.handleUserNameChange}
+                    type="text"
+                    name="username"
+                    placeholder="Enter username"
+                  />
+                </div>
+                <div className="col-3">
+                  <button onClick={() => fetchData(username)} className="btn btn-large btn-success">Search</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
